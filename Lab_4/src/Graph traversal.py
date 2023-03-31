3dbdb6835469970734d941ebceb235acfcc76dab
Sevastian Bajenov <sevabaj@gmail.com>
2023-03-31 09:57:03 +0300

    Fix dfs algorithm. Create manually two input trees.

--- Lab_4/src/Graph traversal.py
+++ Lab_4/src/Graph traversal.py
@@ -1,3 +1,6 @@
+from time import time
+
+
 # Breadth-First Search algorithm
 def bfs(graph, root):
 
@@ -19,17 +22,24 @@ def bfs(graph, root):
 # Depth-First Search algorithm
 def dfs(graph, start, visited=None):
     if visited is None:
-        visited = set()
-    visited.add(start)
+        visited = list()
+    visited.append(start)
 
-    for next in graph[start] - visited:
+    for next in graph[start] - set(visited):
         dfs(graph, next, visited)
 
     return visited
 
 
-if __name__ == '__main__':
-    graph = {0: set([1, 2]), 1: set([2]), 2: set([3]), 3: set([1, 2])}
-    print("Following is Breadth First Traversal: ")
-    bfs(graph, 0)
-    print(dfs(graph, 0))
+def current_time_nanos():
+    return time() * 1000000
+
+
+unbalanced_tree = {0: set([1, 2]), 1: set([3, 4, 5, 11, 12]), 2: set([6, 7]), 3: set([]), 4: set([]),
+                   5: set([]), 6: set([8, 9, 10]), 7: set([]), 8: set([]), 9: set([]), 10: set([14]),
+                   11: set([]), 12: set([13, 17]), 13: set([15, 16]), 14: set([])}
+
+balanced_tree = {0: set([1, 2, 3]), 1: set([4, 5]), 2: set([6, 7]), 3: set([8, 16]), 4: set([]),
+                 5: set([9, 10, 11]), 6: set([]), 7: set([12, 13]), 8: set([14, 15]), 9: set([]), 10: set([]),
+                 11: set([]), 12: set([]), 13: set([]), 14: set([]), 15: set([]), 16: set([])}
+
