b6b7f3e8b6bbbfd957fab3b7d787b7bf28cae697
Sevastian Bajenov <sevabaj@gmail.com>
2023-04-04 14:10:23 +0300

    Finish the algorithms and test their efficiency.

--- Lab_4/src/Graph traversal.py
+++ Lab_4/src/Graph traversal.py
@@ -48,7 +48,8 @@ def current_time_nanos():
 # setting up input data
 unbalanced_tree = {0: set([1, 2]), 1: set([3, 4, 5, 11, 12]), 2: set([6, 7]), 3: set([]), 4: set([]),
                    5: set([]), 6: set([8, 9, 10]), 7: set([]), 8: set([]), 9: set([]), 10: set([14]),
-                   11: set([]), 12: set([13, 17]), 13: set([15, 16]), 14: set([])}
+                   11: set([]), 12: set([13, 17]), 13: set([15, 16]), 14: set([]), 15: set([]), 16: set([]),
+                   17: set([])}
 
 unbalanced_dfs_set = set([5, 6, 14, 16, 17])
 unbalanced_bfs_set = set([2, 4, 7, 8, 12])
@@ -58,10 +59,83 @@ balanced_tree = {0: set([1, 2, 3]), 1: set([4, 5]), 2: set([6, 7]), 3: set([8, 1
                  11: set([]), 12: set([]), 13: set([]), 14: set([]), 15: set([]), 16: set([])}
 
 balanced_dfs_set = set([3, 7, 9, 15, 16])
-balanced_bfs_set = set([4, 6, 8, 11, 16])
+balanced_bfs_set = set([4, 6, 8, 11, 16]),
 
-c = 0
-start_time = current_time_nanos()
-dfs(balanced_tree, 0, balanced_dfs_set, c)
-end_time = current_time_nanos()
-print(end_time - start_time)
+
+# testing the algorithms
+# for the unbalanced tree
+dfs_time_dfs, dfs_time_bfs = 0, 0
+bfs_time_bfs, bfs_time_dfs = 0, 0
+for index in range(1, 11):
+    c = 0
+    
+    # dfs algorithm
+    start_time = current_time_nanos()
+    dfs(unbalanced_tree, 0, unbalanced_dfs_set, c)
+    end_time = current_time_nanos()
+    dfs_time_dfs += (end_time - start_time)
+
+    c = 0
+    start_time = current_time_nanos()
+    dfs(unbalanced_tree, 0, unbalanced_bfs_set, c)
+    end_time = current_time_nanos()
+    dfs_time_bfs += (end_time - start_time)
+
+    # bfs algorithm
+    c = 0
+    start_time = current_time_nanos()
+    bfs(unbalanced_tree, 0, unbalanced_bfs_set, c)
+    end_time = current_time_nanos()
+    bfs_time_bfs += (end_time - start_time)
+
+    c = 0
+    start_time = current_time_nanos()
+    bfs(unbalanced_tree, 0, unbalanced_dfs_set, c)
+    end_time = current_time_nanos()
+    bfs_time_dfs += (end_time - start_time)
+
+print("Unbalanced tree:")
+print(f"DFS searching the DFS set: {round(dfs_time_dfs / index, 4)} nanos")
+print(f"DFS searching the BFS set: {round(dfs_time_bfs / index, 4)} nanos")
+print(f"BFS searching the BFS set: {round(bfs_time_bfs / index, 4)} nanos")
+print(f"BFS searching the DFS set: {round(bfs_time_dfs / index, 4)} nanos")
+
+print('\n\n')
+
+
+# for the balanced tree
+dfs_time_dfs, dfs_time_bfs = 0, 0
+bfs_time_bfs, bfs_time_dfs = 0, 0
+for index in range(1, 11):
+    c = 0
+    
+    # dfs algorithm
+    start_time = current_time_nanos()
+    dfs(balanced_tree, 0, balanced_dfs_set, c)
+    end_time = current_time_nanos()
+    dfs_time_dfs += (end_time - start_time)
+
+    c = 0
+    start_time = current_time_nanos()
+    dfs(balanced_tree, 0, balanced_bfs_set, c)
+    end_time = current_time_nanos()
+    dfs_time_bfs += (end_time - start_time)
+
+    # bfs algorithm
+    c = 0
+    start_time = current_time_nanos()
+    bfs(balanced_tree, 0, balanced_bfs_set, c)
+    end_time = current_time_nanos()
+    bfs_time_bfs += (end_time - start_time)
+
+    c = 0
+    start_time = current_time_nanos()
+    bfs(balanced_tree, 0, balanced_dfs_set, c)
+    end_time = current_time_nanos()
+    bfs_time_dfs += (end_time - start_time)
+
+print("Balanced tree:")
+print(f"DFS searching the DFS set: {round(dfs_time_dfs / index, 4)} nanos")
+print(f"DFS searching the BFS set: {round(dfs_time_bfs / index, 4)} nanos")
+print(f"BFS searching the BFS set: {round(bfs_time_bfs / index, 4)} nanos")
+print(f"BFS searching the DFS set: {round(bfs_time_dfs / index, 4)} nanos")
