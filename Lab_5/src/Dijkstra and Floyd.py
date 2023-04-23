285b368dd4cdeaac9a0a3c1de52723ec89f468cd
Sevastian Bajenov <sevabaj@gmail.com>
2023-04-23 16:11:16 +0300

    Add scripts for plotting analysis results.

--- Lab_5/src/Dijkstra and Floyd.py
+++ Lab_5/src/Dijkstra and Floyd.py
@@ -1,5 +1,6 @@
 import sys
 from random import randint
+from matplotlib.pyplot import plot
 from time import time
 
 
@@ -48,29 +49,26 @@ def Floyd_Warshall_algorithm(vertices, edges):
         for i in range(num_of_vertices):
             for j in range(num_of_vertices):
                 distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j])
-    print(distance)
 
 
+# coefficients for defining the number of edges in a graph
+# thus declaring it as dense or sparse
 dense_coefficient = 80
 sparse_coefficient = 30
 
-'''
+
+# function for creating a dense/sparse graph of a given size
 def generateGraph(size, coef):
-    v = list()
-    e = [[0 for _ in range(size)]] * size
-
-    for i in range(size):
-
-        for j in range(size):
-            if i != j:
-                choice = randint(0, 100)
-                v[i][j] = choice
-        print(v)
-
+    v = random.randint(0, 1, size=(size, size))
+    e = random.randint(1, 5, size=(size, size))
+    
+    for x in range(size):
+        for y in range(size):
+            if x == y:
+                v[x][y] = 0
     print(v)
+    return v, e
 
-generateGraph(5)
-'''
 
 def current_time_millis():
     return time() * 1000
@@ -91,13 +89,13 @@ for index in range(len(input_sizes)):
     Dijkstra_algorithm(vertices, edges)
     end_time = current_time_millis()
 
-    dijkstra_dense.append(round(end_time - start_time), 3)
+    dijkstra_dense.append(round(end_time - start_time, 3))
 
     start_time = current_time_millis()
     Floyd_Warshall_algorithm(vertices, edges)
     end_time = current_time_millis()
     
-    floyd_dense.append(round(end_time - start_time), 3)
+    floyd_dense.append(round(end_time - start_time, 3))
 
 
 # testing on sparse graphs
@@ -108,10 +106,73 @@ for index in range(len(input_sizes)):
     Dijkstra_algorithm(vertices, edges)
     end_time = current_time_millis()
 
-    dijkstra_sparse.append(round(end_time - start_time), 3)
+    dijkstra_sparse.append(round(end_time - start_time, 3))
 
     start_time = current_time_millis()
     Floyd_Warshall_algorithm(vertices, edges)
     end_time = current_time_millis()
     
-    floyd_sparse.append(round(end_time - start_time), 3)
+    floyd_sparse.append(round(end_time - start_time, 3))
+
+
+# plot the obtained results
+# Dijkstra and Floyd-Warshall separately
+plot.figure()
+
+plot.plot(input_sizes, dijkstra_dense, color="red", label="Dense graph")
+plot.plot(input_sizes, dijkstra_sparse, color="blue", label="Sparse graph")
+
+plot.title("Dijkstra Algorithm", color="violet", fontsize=16)
+plot.legend(loc='upper left')
+
+plot.xlabel("n", color="violet", fontsize=14)
+plot.ylabel("T(millis)", color="violet", fontsize=14)
+
+plot.grid()
+plot.show()
+
+
+plot.figure()
+
+plot.plot(input_sizes, floyd_dense, color="red", label="Dense graph")
+plot.plot(input_sizes, floyd_sparse, color="blue", label="Sparse graph")
+
+plot.title("Floyd-Warshall Algorithm", color="violet", fontsize=16)
+plot.legend(loc='upper left')
+
+plot.xlabel("n", color="violet", fontsize=14)
+plot.ylabel("T(millis)", color="violet", fontsize=14)
+
+plot.grid()
+plot.show()
+
+
+# comparative analysis of Dijkstra and Floyd-Warshall algorithms
+plot.figure()
+
+plot.plot(input_sizes, dijkstra_dense, color="red", label="Dijkstra")
+plot.plot(input_sizes, floyd_dense, color="blue", label="Floyd-Warshall")
+
+plot.title("Dense graphs", color="violet", fontsize=16)
+plot.legend(loc='upper left')
+
+plot.xlabel("n", color="violet", fontsize=14)
+plot.ylabel("T(millis)", color="violet", fontsize=14)
+
+plot.grid()
+plot.show()
+
+
+plot.figure()
+
+plot.plot(input_sizes, dijkstra_sparse, color="red", label="Dijkstra")
+plot.plot(input_sizes, floyd_sparse, color="blue", label="Floyd-Warshall")
+
+plot.title("Sparse graphs", color="violet", fontsize=16)
+plot.legend(loc='upper left')
+
+plot.xlabel("n", color="violet", fontsize=14)
+plot.ylabel("T(millis)", color="violet", fontsize=14)
+
+plot.grid()
+plot.show()
